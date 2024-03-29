50c933c638d2b5d0b4e50a65f604c8260d74f3d5
dilpreet <dilpreetbajwa1@gmail.com>
2024-03-29 16:39:38 -0400

    FooterSection

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Menu from './components/Menu';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
     <Nav />
-    <Main />
+    <Main/>
     <Menu/>
+    <Footer/>
     </>
   );
 }
